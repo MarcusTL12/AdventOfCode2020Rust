7073aeb66f5d6a982916110e6c04023e79fb488e
MarcusTL12 <marcuspaafjellet@gmail.com>
2020-12-01 06:31:38 +0100

    Day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,42 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use itertools::Itertools;
+
 pub const PARTS: [fn(); 2] = [part1, part2];
 
-fn part1() {
-    println!("Not yet!");
+fn load_input(filename: &str) -> Vec<u64> {
+    BufReader::new(File::open(filename).unwrap())
+            .lines()
+            .map(|l| l.unwrap().parse().unwrap())
+            .collect()
 }
 
-fn part2() {}
+fn part1() {
+    let numbers = load_input("inputfiles/day1/input.txt");
+    //
+    let (a, b) = numbers
+        .iter()
+        .cartesian_product(numbers.iter())
+        .filter(|&(a, b)| a + b == 2020)
+        .next()
+        .unwrap();
+    //
+    println!("{}", a * b);
+}
+
+fn part2() {
+    let numbers = load_input("inputfiles/day1/input.txt");
+    //
+    let ((a, b), c) = numbers
+        .iter()
+        .cartesian_product(numbers.iter())
+        .cartesian_product(numbers.iter())
+        .filter(|&((a, b), c)| a + b + c == 2020)
+        .next()
+        .unwrap();
+    //
+    println!("{}", a * b * c);
+}
